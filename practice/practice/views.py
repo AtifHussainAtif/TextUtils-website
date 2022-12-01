76dc3fa9596b38b27dbc0490ca56aa8eaec6ad98
Atif Hussain <104657872+AtifHussainAtif@users.noreply.github.com>
2022-12-02 00:16:46 +0500

    bootstrap practice

--- practice/practice/views.py
+++ practice/practice/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index2.html')
 
 def ex1(request):
     sites = ['''<h1>For Entertainment </h1><a href = "https://www.youtube.com" >youtube video</a>''',
@@ -17,6 +17,8 @@ def analyze(request):
     removepunc = request.GET.get('removepunc', 'off')
     fullcaps = request.GET.get('fullcaps', 'off')
     newlineremover= request.GET.get('newlineremover', 'off')
+    extraspaceremover = request.GET.get('extraspaceremover', 'off')
+    charcount = request.GET.get('charcount', 'off')
 
     if removepunc == "on":
          punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -26,14 +28,32 @@ def analyze(request):
                 analyzed = analyzed +char
 
          params = {'purpose' : 'remove punctuation', 'analyzed_text' : analyzed}
-         return render(request, 'analyze.html', params)
+         return render(request, 'analyze2.html', params)
     elif(fullcaps == 'on'):
         analyzed = ""
         for char in djtext:
             analyzed = analyzed + char.upper()
 
         params = {'purpose' : 'remove punctuation', 'analyzed_text' : analyzed}
-        return render(request, 'analyze.html', params)
+        return render(request, 'analyze2.html', params)
+
+    elif(charcount =='on'):
+        analyzed = len(djtext)
+        
+        
+
+        params = {'purpose' : 'ccharacter count', 'analyzed_text' : analyzed}
+        return render(request, 'analyze2.html', params)
+
+    elif(charcount=='on'):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1] == " "):
+               analyzed = analyzed + char
+
+        params = {'purpose' : 'extra space remover', 'analyzed_text' : analyzed}
+        return render(request, 'analyze2.html', params)
+
     elif(newlineremover=='on'):
         analyzed = ""
         for char in djtext:
@@ -41,6 +61,9 @@ def analyze(request):
                 analyzed = analyzed + char
 
         params = {'purpose' : 'New Line REmover', 'analyzed_text' : analyzed}
-        return render(request, 'analyze.html', params)
+        return render(request, 'analyze2.html', params)
+
+
+
     else:
         return HttpResponse("Error 404")
