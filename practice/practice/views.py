2f0fb4fc623858b1555cfb016d781bd0b370db50
Atif Hussain <104657872+AtifHussainAtif@users.noreply.github.com>
2022-12-01 12:42:02 +0500

    new website practice

---
+++ practice/practice/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def ex1(request):
+    sites = ['''<h1>For Entertainment </h1><a href = "https://www.youtube.com" >youtube video</a>''',
+             '''<h1>For Interaction </h1><a href = "https://www.facebook.com" >Facebook</a>''',
+             '''<h1>For Insight   </h1><a href = "https://www.ted.com/talks" >Ted Talk</a>''',
+             '''<h1>For Internship   </h1><a href="https://internshala.com" >Intenship</a>''',
+             ]
+    return HttpResponse((sites))
+    
+def analyze(request):
+    djtext = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    print(djtext)
+    print(removepunc)
+    if removepunc == "on":
+         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+         analyzed = ''
+         for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed +char
+
+         params = {'purpose' : 'remove punctuation', 'analyzed_text' : analyzed}
+         return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse("Error")
